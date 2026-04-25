52b870b9a90e7ac6a91f740fd034116e4b0a08c2
barti06 <bartipdl1@gmail.com>
2026-04-25 20:37:39 -0300

    remove need for sudo on headsetcontrol

--- modules/features/headset-control.nix
+++ modules/features/headset-control.nix
@@ -7,5 +7,9 @@
     in {
         environment.systemPackages = [ customHeadsetControl ];
         services.udev.packages = [ customHeadsetControl ];
+        services.udev.extraRules = ''
+            SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="2a08", MODE="0666"
+            SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="2a09", MODE="0666"
+        '';
     };
 }
